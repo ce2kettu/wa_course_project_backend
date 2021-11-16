64e4897e2815348abed277583c1381bff7481383
ce2kettu <teemu.ket@gmail.com>
2021-11-16 18:15:19 +0200

    Add routes for login, register, and profile

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const User = require('./models/user');
 
 const mongoDB = process.env.MONGOURL || 'mongodb://localhost:27017/testdb';
 mongoose.connect(mongoDB);
-mongoose.Promise = Promise;
+mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('connected', () => console.log('Connected to MongoDB'));
 db.on('error', () => console.error('MongoDB connection error'));
 
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+var options = {}
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = process.env.JWT_SECRET;
+passport.use(new JwtStrategy(options, (jwtPayload, done) => {
+    User.findById(jwtPayload.id, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
 
-var app = express();
+        return done(null, user || false);
+    }).populate('+isAdmin +email');
+}));
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const commentsRouter = require('./routes/comments');
+const votesRouter = require('./routes/votes');
+
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/posts', postsRouter);
+app.use('/api/comments', commentsRouter);
+app.use('/api/votes', votesRouter);
 
 // not found
 app.use(function (req, res, next) {
@@ -36,7 +61,6 @@ app.use(function (err, req, res, next) {
     const message = isDev ? err.message : "Internal server error";
     const error = isDev ? err : {};
 
-    // render the error page
     res.status(err.status || 500).json({ message });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,114 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/profile/:userId',
+  passport.authenticate('jwt', { session: false }),
+  param('userId').exists().isMongoId(),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ success: false, message: 'Bad Request', errors: errors.array() });
+      }
+
+      const user = await User.findById(req.params.userId).populate('+bio');
+
+      return res.json({ success: true, profile: user });
+    } catch {
+      res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+  });
+
+router.get('/profile',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const user = await User.findById(req.user._id).populate('+bio +email');
+      return res.json({ success: true, profile: user });
+    } catch {
+      res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+  });
+
+router.post('/register',
+  body('email').isEmail().normalizeEmail(),
+  body('password').isStrongPassword(),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ success: false, message: 'Bad Request', errors: errors.array() });
+      }
+
+      const user = await User.findOne({ email: req.body.email });
+
+      if (user) {
+        return res.status(403).json({ success: false, message: 'Email already in use' });
+      } else {
+        const newUser = new User();
+
+        bcrypt.hash(req.body.password, 8, async (err, hash) => {
+          newUser.email = req.body.email;
+          newUser.password = hash;
+          await newUser.save();
+          return res.json({ success: true });
+        });
+      }
+
+    } catch (err) {
+      return res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+  });
+
+router.post('/login',
+  body('email').exists(),
+  body('password').exists(),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ success: false, message: 'Bad Request', errors: errors.array() });
+      }
+
+      const user = await User.findOne({ email: req.body.email }).select('+password +email');
+
+      if (!user) {
+        return res.status(403).json({ success: false, message: 'Invalid credentials' });
+      } else {
+        bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+          if (err) throw err;
+
+          if (isMatch) {
+            const jwtPayload = {
+              id: user._id,
+              email: user.email,
+            }
+
+            jwt.sign(
+              jwtPayload,
+              process.env.JWT_SECRET,
+              {
+                expiresIn: '30d'
+              },
+              (err, token) => {
+                res.json({ success: true, token });
+              }
+            );
+          } else {
+            return res.status(403).json({ success: false, message: 'Invalid credentials' });
+          }
+        });
+      }
+    } catch (err) {
+      res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+  });
 
 module.exports = router;
