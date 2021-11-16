b2e41c959c759d8c4e310db8f88ff3c8ea2698e4
ce2kettu <teemu.ket@gmail.com>
2021-11-16 18:15:43 +0200

    Add routes for posts

---
+++ routes/posts.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const { body, param, validationResult } = require("express-validator");
+const router = express.Router();
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+router.get('/all', async (req, res, next) => {
+    try {
+        const posts = await Post.find({}).populate('user');
+        return res.json({ success: true, posts });
+    } catch (err) {
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+});
+
+router.get('/:postId',
+    param('postId').exists().isMongoId(),
+    async (req, res, next) => {
+        try {
+            const post = await Post.findById(req.params.postId).populate('user', 'comments')
+                .populate('comments.user');
+
+            if (!post) {
+                return res.status(400).json({ success: false, message: "Bad Request" });
+            } else {
+                return res.json({ success: true, post });
+            }
+        } catch (err) {
+            return res.status(500).json({ success: false, message: "Internal Server Error" });
+        }
+    });
+
+router.post('/new',
+    passport.authenticate('jwt', { session: false }),
+    body('title').exists(),
+    body('body').exists(),
+    async (req, res, next) => {
+        try {
+            const post = new Post();
+            post.title = req.body.title;
+            post.body = req.body.body;
+            post.user = req.user._id;
+            await post.save();
+            return res.json({ success: true, post });
+        } catch (err) {
+            return res.status(500).json({ success: false, message: "Internal Server Error" });
+        }
+    });
+
+router.put('/:postId/edit',
+    passport.authenticate('jwt', { session: false }),
+    param('postId').exists().isMongoId(),
+    body('title').exists(),
+    body('body').exists(),
+    async (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ success: false, message: "Bad Request", errors: errors.array() });
+            }
+
+            const post = await Post.findById(req.params.postId);
+
+            if (!post) {
+                return res.status(400).json({ success: false, message: "Bad Request" });
+            }
+
+            if (req.user._id == post.user || req.user.isAdmin) {
+                post.title = req.body.title;
+                post.body = req.body.body;
+                await post.save();
+                res.json({ success: true, post });
+            } else {
+                return res.status(400).json({ success: false, message: "Bad Request" });
+            }
+        } catch (err) {
+            return res.status(500).json({ success: false, message: "Internal Server Error" });
+        }
+    });
+
+router.delete('/:postId/delete',
+    passport.authenticate('jwt', { session: false }),
+    param('postId').exists().isMongoId(),
+    async (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ success: false, message: "Bad Request", errors: errors.array() });
+            }
+
+            const post = await Post.findById(req.params.postId);
+
+            if (!post) {
+                return res.status(400).json({ success: false, message: "Bad Request" });
+            }
+
+            if (req.user.isAdmin) {
+                const postId = post._id;
+                await post.delete();
+                Comment.remove({ post: postId }).exec();
+                res.json({ success: true });
+            } else {
+                return res.status(400).json({ success: false, message: "Bad Request" });
+            }
+        } catch (err) {
+            return res.status(500).json({ success: false, message: "Internal Server Error" });
+        }
+    });
+
+module.exports = router;
